80b8bdbe63acb7cd244a6a34fe104453594de54a
Ana Segarra <anasegarraber@gmail.com>
2020-03-28 21:17:30 +0100

    Add prototype user model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const schema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    image: String,
+    email: String,
+    isAdmin: { type: Boolean, default: false },
+    gameLists: [
+      {
+        listType: { enum: ['wishlist', 'gamesPlayed'] },
+        list: { type: ObjectId, ref: 'list' }
+      }
+    ],
+    friends: [{ type: ObjectId, ref: 'user' }]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('user', schema);
