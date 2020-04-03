fc9827b0bff4458c528274e064aaad7ecde3d722
Ana Segarra <anasegarraber@gmail.com>
2020-04-03 13:33:13 +0200

    Add route to make a request

--- server/routes/requests.router.js
+++ server/routes/requests.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Request = require('../models/Request');
+const Game = require('../models/Game');
 
 // GET route - retrieve requests from db
 router.get('/', async (req, res, next) => {
@@ -26,6 +27,58 @@ router.get('/', async (req, res, next) => {
     return res
       .status(500)
       .json({ message: 'Internal server error fetching requests from database' });
+  }
+});
+
+// POST route - add a request
+router.post('/', async (req, res, next) => {
+  const {
+    name,
+    description,
+    image,
+    releaseYear,
+    platforms,
+    linkToBuy,
+    genres,
+    ESRB,
+    company
+  } = req.body;
+
+  try {
+    // status 400 if there's already a pending request for that game
+    const requestRegistered = await Request.findOne({ 'content.name': name, status: 'Pending' });
+    if (requestRegistered) {
+      console.log('Request already being processed', requestRegistered);
+      return res.status(400).json({ message: `A request for ${name} is already being processed` });
+    }
+
+    // status 400 if the game was already request and approved or is already in the db
+    const requestApproved = await Request.findOne({ 'content.name': name, status: 'Approved' });
+    const gameInDb = await Game.findOne({ name });
+    if (requestApproved || gameInDb) {
+      console.log('Game already in db', requestRegistered, gameInDb);
+      return res.status(400).json({ message: `${name} is already in the games list!` });
+    }
+
+    const newRequest = await Request.create({
+      requestedBy: req.user.id,
+      content: {
+        name,
+        description,
+        image,
+        releaseYear,
+        platforms,
+        linkToBuy,
+        genres,
+        ESRB,
+        company
+      }
+    });
+    console.log('Request made', newRequest);
+    return res.status(201).json({ message: 'Request successfully made', request: newRequest });
+  } catch (error) {
+    console.log('Error creating a request', error);
+    return res.status(500).json({ message: 'Internal server error making a request' });
   }
 });
 
