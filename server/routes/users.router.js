513c660aee2055927d59a5793f408507e9dab496
Ana Segarra <anasegarraber@gmail.com>
2020-04-04 18:46:15 +0200

    Include route to add friends

--- server/routes/users.router.js
+++ server/routes/users.router.js
@@ -45,4 +45,39 @@ router.get('/search', async (req, res, next) => {
   }
 });
 
+// GET route - retrieve user's friends
+// POST route - add new friend
+router.post('/friends', async (req, res, next) => {
+  const { user_id } = req.body;
+  try {
+    // check if user to friend exists in db
+    const userExists = await User.findById(user_id);
+    if (!userExists) {
+      console.log('There are no users with an id of ', user_id);
+      return res.status(400).json({ message: `Couldn't find that user` });
+    }
+    // add to friend list if it's not already friended
+    const isFriend = req.user.friends.includes(user_id);
+    if (!isFriend) {
+      const userUpdated = await User.findByIdAndUpdate(
+        req.user.id,
+        { $push: { friends: user_id } },
+        { new: true }
+      );
+      console.log('New friend added', userUpdated);
+      return res
+        .status(200)
+        .json({ message: 'New friend added successfully', friends: userUpdated });
+    }
+
+    console.log(`User with id ${user_id} is already a friend`);
+    return res.status(400).json({ message: 'You are friends already' });
+  } catch (error) {
+    console.log('Error adding a new friend', error);
+    return res.status(500).json({ message: 'Internal server error adding a new friend' });
+  }
+});
+
+// DELETE route - delete friend
+
 module.exports = router;
