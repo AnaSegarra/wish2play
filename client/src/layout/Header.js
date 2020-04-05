d3f014488243d2a487ac8568b3f7b02f24f6324c
Ana Segarra <anasegarraber@gmail.com>
2020-04-05 21:38:47 +0200

    Style navbar

--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -1,22 +1,35 @@
 // dependencies
 import React, { useContext } from 'react';
 import { GameController } from '@styled-icons/entypo';
+import { UserCircle } from '@styled-icons/boxicons-solid';
+import { LogOut } from '@styled-icons/boxicons-regular';
 
 // local modules
 import { AuthContext } from '../contexts/authContext';
+import { logout } from '../services/authService';
 
 // styled components
-import { Navbar } from '../StyledComponents/Navbar';
+import { Navbar, SubBar } from '../StyledComponents/Navbar';
 
 export const Header = () => {
-  const { user } = useContext(AuthContext);
-  console.log('en el header', user);
+  const { user, setUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+  };
+
   return (
     <header>
       <Navbar>
         <GameController size="25" />
         <a>Games</a>
       </Navbar>
+      <SubBar user={user}>
+        <p>{user && user.username}</p>
+        <UserCircle size="25" />
+        <LogOut size="25" onClick={handleLogout} />
+      </SubBar>
     </header>
   );
 };
