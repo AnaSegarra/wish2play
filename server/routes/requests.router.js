ca843144e4f092bfbf1f8037ec245adbb9f3827b
Ana Segarra <anasegarraber@gmail.com>
2020-04-07 23:53:14 +0200

    Clean request routes

--- server/routes/requests.router.js
+++ server/routes/requests.router.js
@@ -2,25 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Request = require('../models/Request');
 const Game = require('../models/Game');
+const { isLoggedIn, checkUserRole } = require('../lib/authMW');
+const { isEmptyField } = require('../lib/validatorMW');
 
 // GET route - retrieve requests from db
-router.get('/', async (req, res, next) => {
+router.get('/', isLoggedIn(), async (req, res, next) => {
   try {
-    const requests = await Request.find();
-
+    const query = Request.find();
     // retrieve every request from db if admin
     if (req.user.isAdmin) {
-      return res
-        .status(200)
-        .json({ message: 'Requests fetched successfully', results: requests.length, requests });
+      const requests = await query;
+      return res.json({
+        message: 'Requests fetched successfully',
+        results: requests.length,
+        requests
+      });
     }
 
     // retrieve only user's requests otherwise
-    const userRequests = await Request.find({ requestedBy: req.user.id });
-    return res.status(200).json({
+    const requests = await query.where('requestedBy').equals(req.user.id);
+    return res.json({
       message: `User's requests fetched successfully`,
-      results: userRequests.length,
-      userRequests
+      results: requests.length,
+      requests
     });
   } catch (error) {
     console.log('Error retrieving requests', error);
@@ -31,50 +35,23 @@ router.get('/', async (req, res, next) => {
 });
 
 // POST route - add a request
-router.post('/', async (req, res, next) => {
-  const {
-    name,
-    description,
-    image,
-    releaseYear,
-    platforms,
-    linkToBuy,
-    genres,
-    ESRB,
-    company
-  } = req.body;
-
+router.post('/', isLoggedIn(), isEmptyField('name'), async (req, res, next) => {
   try {
     // status 400 if there's already a pending request for that game
     const requestRegistered = await Request.findOne({ 'content.name': name, status: 'Pending' });
-    if (requestRegistered) {
-      console.log('Request already being processed', requestRegistered);
+    if (requestRegistered)
       return res.status(400).json({ message: `A request for ${name} is already being processed` });
-    }
 
-    // status 400 if the game was already request and approved or is already in the db
+    // status 400 if the game was already requested and approved or is already in the db
     const requestApproved = await Request.findOne({ 'content.name': name, status: 'Approved' });
     const gameInDb = await Game.findOne({ name });
-    if (requestApproved || gameInDb) {
-      console.log('Game already in db', requestRegistered, gameInDb);
+    if (requestApproved || gameInDb)
       return res.status(400).json({ message: `${name} is already in the games list!` });
-    }
 
     const newRequest = await Request.create({
       requestedBy: req.user.id,
-      content: {
-        name,
-        description,
-        image,
-        releaseYear,
-        platforms,
-        linkToBuy,
-        genres,
-        ESRB,
-        company
-      }
+      content: req.body
     });
-    console.log('Request made', newRequest);
     return res.status(201).json({ message: 'Request successfully made', request: newRequest });
   } catch (error) {
     console.log('Error creating a request', error);
@@ -82,22 +59,15 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', checkUserRole(), async (req, res, next) => {
   const { id } = req.params;
-  const { status } = req.body;
-
-  console.log('status', status, 'id', id);
 
   try {
-    const request = await Request.findByIdAndUpdate(id, { status }, { new: true });
+    const request = await Request.findByIdAndUpdate(id, req.body, { new: true });
 
-    if (!request) {
-      console.log(`Couldn't find a request with an id of ${id}`);
-      return res.status(404).json({ message: 'Request not found' });
-    }
+    if (!request) return res.status(404).json({ message: 'Request not found' });
 
-    console.log('Update request status', request);
-    return res.status(200).json({ message: 'Request status successfully updated', request });
+    return res.json({ message: 'Request status successfully updated', request });
   } catch (error) {
     console.log('Error updating request status', error);
     return res.status(500).json({ message: 'Internal server error updating request status' });
