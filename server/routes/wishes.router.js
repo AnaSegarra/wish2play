7da2c5d6516f7cc44491f02bc56e75368f8e8f12
Ana Segarra <anasegarraber@gmail.com>
2020-04-08 12:33:36 +0200

    Add route to update wish status

--- server/routes/wishes.router.js
+++ server/routes/wishes.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const Wish = require('../models/Wish');
 const Game = require('../models/Game');
-const { isLoggedIn, isFriend } = require('../lib/authMW');
+const { isLoggedIn, isFriend, checkOwnership } = require('../lib/authMW');
 
 // GET route - retrieve wishlist from a user
 router.get('/:id/wishlist', async (req, res, next) => {
@@ -36,6 +36,9 @@ router.get('/:id/wishlist', async (req, res, next) => {
 router.post('/wishlist', isLoggedIn(), async (req, res, next) => {
   const { game_id } = req.body;
   try {
+    const gameExists = await Game.findById(game_id);
+    if (!gameExists) return res.status(400).json({ message: 'Game not found' });
+
     // check if game is already in wishlist
     const isGameWished = await Wish.find({ owner: req.user.id, game: game_id });
     if (isGameWished.length !== 0)
@@ -54,6 +57,18 @@ router.post('/wishlist', isLoggedIn(), async (req, res, next) => {
     return res
       .status(500)
       .json({ message: 'Internal server error adding a game to games played list' });
+  }
+});
+
+// PUT route - update wish status
+router.put('/wishlist/:id', isLoggedIn(), checkOwnership(Wish, 'owner'), async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const wishUpdated = await Wish.findByIdAndUpdate(id, req.body, { new: true });
+    return res.json({ message: 'Wish status updated', wishUpdated });
+  } catch (error) {
+    console.log('Error editing wish status', error);
+    return res.status(500).json({ message: 'Internal server error editing wish status' });
   }
 });
 
@@ -86,12 +101,14 @@ router.post('/reserved-wishes', isFriend(), async (req, res, next) => {
 });
 
 // DELETE route - remove wish from wishlist
-router.delete('/wishlist/:id', isLoggedIn(), async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    // check whether is user's wish
-    const wish = await Wish.findOne({ _id: id, owner: req.user.id });
-    if (wish) {
+router.delete(
+  '/wishlist/:id',
+  isLoggedIn(),
+  checkOwnership(Wish, 'owner'),
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const wish = await Wish.findById(id);
       // remove wish from wishlist and from db
       const userUpdated = await User.findByIdAndUpdate(
         req.user.id,
@@ -100,26 +117,25 @@ router.delete('/wishlist/:id', isLoggedIn(), async (req, res, next) => {
         },
         { new: true }
       );
-      await Wish.findByIdAndDelete(id);
 
-      // remove wish from friend's reserved wishes list
+      // remove wish from user that reserved it
       if (wish.status === 'Reserved') {
         await User.findOneAndUpdate(
-          { _id: { $in: req.user.friends }, reservedWishes: id },
+          { reservedWishes: id },
           { $pull: { reservedWishes: id } },
           { new: true }
         );
       }
 
-      return res.json({ message: 'Wish removed succesfully from wishlist', userUpdated });
-    }
+      await wish.delete();
 
-    return res.status(400).json({ message: 'Wish is not included in your wishlist' });
-  } catch (error) {
-    console.log('Error removing wish from wishlist', error);
-    return res.status(500).json({ message: 'Internal server error deleting wish' });
+      return res.json({ message: 'Wish removed succesfully from wishlist', userUpdated });
+    } catch (error) {
+      console.log('Error removing wish from wishlist', error);
+      return res.status(500).json({ message: 'Internal server error deleting wish' });
+    }
   }
-});
+);
 
 // DELETE route - delete friend's wish from reservedWishes
 router.delete('/reserved-wishes/:id', isLoggedIn(), async (req, res, next) => {
