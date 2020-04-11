9f81c0d2f445e8b87b3ba742b9fa963e4151d592
Ana Segarra <anasegarraber@gmail.com>
2020-04-12 01:00:52 +0200

    Animate reviews and include BSO container

---
+++ client/src/pages/GameDetail/GameBSO.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const BSO = props => {
+  return <div></div>;
+};
--- client/src/pages/GameDetail/GameReviews.js
+++ client/src/pages/GameDetail/GameReviews.js
@@ -2,31 +2,31 @@ import React, { useContext } from 'react';
 import { Rating } from '@material-ui/lab';
 import { Grid } from '@material-ui/core';
 import { ReviewsContainer, Review } from '../../StyledComponents/GameDetail.styled';
-import { ThemeContext } from 'styled-components';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export const GameReviews = ({ reviews }) => {
-  const theme = useContext(ThemeContext);
-
   return (
     <ReviewsContainer elevation={3}>
       <p>Players Reviews</p>
       <Grid container spacing={3}>
         {reviews.map((review, i) => {
           return (
-            <Grid item xs={3} key={i}>
-              <Review>
-                <div className="stats">
-                  <p>{review.author.username}</p>
-                  <Rating
-                    name="half-rating-read"
-                    value={review.rating}
-                    readOnly
-                    precision={0.5}
-                    size="small"
-                  />
-                </div>
-                <p>{review.content}</p>
-              </Review>
+            <Grid item lg={3} md={4} sm={6} xs={12} key={i}>
+              <ScrollAnimation animateIn="fadeIn">
+                <Review>
+                  <div className="stats">
+                    <p>{review.author.username}</p>
+                    <Rating
+                      name="half-rating-read"
+                      value={review.rating}
+                      readOnly
+                      precision={0.5}
+                      size="small"
+                    />
+                  </div>
+                  <p>{review.content}</p>
+                </Review>
+              </ScrollAnimation>
             </Grid>
           );
         })}
