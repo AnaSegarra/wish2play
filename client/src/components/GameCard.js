82e057d983bf7e25afd00bfcf24096635d370f20
Ana Segarra <anasegarraber@gmail.com>
2020-04-11 20:58:11 +0200

    Implement animation on scroll

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -12,6 +12,7 @@ import {
 } from '@material-ui/core';
 import { Rating } from '@material-ui/lab';
 import { ThemeContext } from 'styled-components';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 // local modules
 import { BottomCard, TopCard } from '../StyledComponents/Games.styled';
@@ -29,26 +30,28 @@ export const GameCard = ({ name, image, _id, genres, totalRating }) => {
   const classes = useStyles(theme);
 
   return (
-    <Grid item xs={4}>
-      <Card elevation={3}>
-        <CardActionArea>
-          <CardMedia style={{ height: 500 }} image={image} title="Contemplative Reptile" />
-          <CardContent>
-            <TopCard>
-              <p>{name}</p>
-              <Rating name="half-rating-read" value={totalRating} readOnly precision={0.5} />
-            </TopCard>
-          </CardContent>
-        </CardActionArea>
-        <BottomCard theme={theme}>
-          <div>
-            {genres.map((genre, i) => (
-              <Chip key={i} label={genre} className={classes.chip} />
-            ))}
-          </div>
-          <Link to={`/games/${_id}`}>See details</Link>
-        </BottomCard>
-      </Card>
+    <Grid item lg={4} md={6} xs={12}>
+      <ScrollAnimation animateIn="fadeIn">
+        <Card elevation={3}>
+          <CardActionArea>
+            <CardMedia style={{ height: 500 }} image={image} title={name} />
+            <CardContent>
+              <TopCard>
+                <p>{name}</p>
+                <Rating name="half-rating-read" value={totalRating} readOnly precision={0.5} />
+              </TopCard>
+            </CardContent>
+          </CardActionArea>
+          <BottomCard theme={theme}>
+            <div>
+              {genres.map((genre, i) => (
+                <Chip key={i} label={genre} className={classes.chip} />
+              ))}
+            </div>
+            <Link to={`/games/${_id}`}>See details</Link>
+          </BottomCard>
+        </Card>
+      </ScrollAnimation>
     </Grid>
   );
 };
