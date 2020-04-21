5050aab04367c91cc23e94678d9e6e48dedf337c
Ana Segarra <anasegarraber@gmail.com>
2020-04-21 18:22:40 +0200

    Include helper function for request sorting

--- client/src/helpers/listsHelpers.js
+++ client/src/helpers/listsHelpers.js
@@ -6,3 +6,6 @@ export const arrMapped = list => (list.length > 0 ? list.map(({ game }) => game.
 
 // sort by game name
 export const sortByName = list => list.sort((a, b) => a.game.name.localeCompare(b.game.name));
+
+// sort by status
+export const sortByStatus = list => list.sort((a, b) => a.status.localeCompare(b.status));
