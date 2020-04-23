9fb13f341e99e20c9c192de8f8df1678b7108fa2
Ana Segarra <anasegarraber@gmail.com>
2020-04-23 11:47:37 +0200

    Fix soundtrack when album type single is retrieve

--- client/src/services/spotifyService.js
+++ client/src/services/spotifyService.js
@@ -13,12 +13,17 @@ export const fetchAlbumID = async (game, token) => {
   try {
     const { data } = await axios.get(`${process.env.API_URL}/spotify`, { params: { game, token } });
     const album = data.albums.items[0];
-    const albumID = album.id;
-    const images = album.images;
-    const response = await axios.get(`${process.env.API_URL}/spotify/soundtrack`, {
-      params: { albumID, token }
-    });
-    return { tracks: response.data.items, cover: images };
+    // search for soundtrack only if found item is of type album
+    if (album.album_type === 'album') {
+      console.log('es del tipo album');
+      const albumID = album.id;
+      const images = album.images;
+      const response = await axios.get(`${process.env.API_URL}/spotify/soundtrack`, {
+        params: { albumID, token }
+      });
+      return { tracks: response.data.items, cover: images };
+    }
+    console.log('no es del tipo album');
   } catch (error) {
     throw new Error('Soundtrack not found');
   }
